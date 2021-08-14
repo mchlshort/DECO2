4270bb756e8f0b07cd185126b1835cc299e251e2
Purusothmn <purusothmn.nair@gmail.com>
2021-08-14 14:55:48 +0800

    Python Run File
    This file contains the run code in Python to optimise the decarbonisation software. A user should alter the directory according to one's requirement beforehand.

---
+++ Optimal_Decarbonisation_Run_File.py
@@ -0,0 +1,20 @@
+'''
+Created on 4th August 2021
+
+This script presents the mathematical optimisation formulation for an energy planning scenario in a specific geographical region or district.
+Renewable energy and fossil-based sources make up the power generation in a specific geographical region or district. 
+Each period consists of its respective energy demand and CO2 emission limit.
+EP-NETs and EC-NETs may be utilised to achieve the CO2 emission limit for each period.
+This energy planning mathematical formulation may be optimised subject to either a mininum cost or minimum emission objective function.
+
+@author: Purusothmn, Dr Michael Short, Prof Dr Dominic C.Y.F.
+'''
+import os 
+'''
+The user would be required to alter the directory name according to one's requirement
+'''
+path = os.chdir(r'C:/Users/LENOVO/OneDrive - University of Nottingham Malaysia/The University of Nottingham/BC COP26 Trilateral Research Initiative/BCCOP26TrilateralProject') 
+
+cwd = os.getcwd()
+
+import Optimal_Decarbonisation_Model_Python
